37d5e8645a8622551ac5018f7c0a0f2f3d4c7e12
Achmad Fawait <fawait626@gmail.com>
2024-11-30 00:26:59 +0700

    fix: higlight map

--- src/modules/dashboard/views/Partial/HiglightMap.vue
+++ src/modules/dashboard/views/Partial/HiglightMap.vue
@@ -9,8 +9,11 @@ const chartOptions = ref<Record<string, any>>({
   chart: {
     map: null,
   },
+  legend: {
+    enabled: false
+  },
   title: {
-    text: "Wilayah Indonesia",
+    text: "",
   },
   mapNavigation: {
     enabled: true,
@@ -35,7 +38,7 @@ const chartOptions = ref<Record<string, any>>({
       name: "Wilayah",
       states: {
         hover: {
-          color: "#03045e",
+          color: "#FFB200",
         },
       },
       dataLabels: {
@@ -80,8 +83,8 @@ async function loadMapData() {
             "Gorontalo",
             "Maluku",
           ].includes(item.properties.state)
-            ? "#03045e"
-            : "#00b4d8",
+            ? "#0070c0"
+            : ["Kalimantan Timur", "Papua", "Maluku Utara", "Sulawesi Selatan", "Riau", "Lampung", "Nusa Tenggara Timur"].includes(item.properties.state) ? "#00B0F0" : "#2AB6C0",
           name: item.properties.state,
         };
       }
@@ -103,18 +106,25 @@ onMounted(() => {
 </script>
 
 <template>
-  <highcharts
-    :constructorType="'mapChart'"
-    class="hc"
-    :options="chartOptions"
-    ref="chart"
-  >
-  </highcharts>
+  <div class="relative">
+    <div class="py-1 px-2 bg-[#2AB6C0] transform -skew-x-12 max-w-[40%] text-center text-white opacity-[0.5] mx-auto">
+      <h1 class="text-3xl font-bold">UNIT PEMBANGKIT PT PLN INDONESIA POWER</h1>
+    </div>
+    <highcharts :constructorType="'mapChart'" class="hc" :options="chartOptions" ref="chart"></highcharts>
+    <div class="absolute z-50 bottom-20 left-10 text-[#2AB6C0] max-w-[400px]">
+      <h1 class="text-2xl font-bold my-3 italic">GENERATE SCOPE</h1>
+      <p class="text-justify indent-20">Aplikasi ini merupakan aplikasi berbasis web yang digunakan untuk membuat scope
+        overhoul
+        berdasarkan kondisi
+        asset pembangkit yang dilengkapi dengan fitur visualisasi proses pekerjaan dan data penunjang, sehingga
+        memudahkan planner untuk memastikan ketepatan durasi dan budgeting.</p>
+    </div>
+  </div>
 </template>
 
 <style>
 .hc {
   width: 100%;
-  height: 100vh;
+  height: 80vh;
 }
 </style>
