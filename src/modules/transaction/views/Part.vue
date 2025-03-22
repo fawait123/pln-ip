82252f1e972df2a969a189546b6f82919ae4107a
rayhan <ahmadraihan015@gmail.com>
2025-03-22 16:15:50 +0700

    fix: added location and size

--- src/modules/transaction/views/Part.vue
+++ src/modules/transaction/views/Part.vue
@@ -58,6 +58,8 @@ const {
             project_uuid: item.project_uuid,
             additional_scope_uuid: item.additional_scope_uuid,
             note: item.note,
+            location: item.location,
+            size: item.size,
           };
         }) || [];
       entitiesPart.value = new_arr;
